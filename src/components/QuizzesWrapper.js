5c783611466b776896412d9fdf794ba9c2bedf2e
werdien jihed <informatique.jihed@gmail.com>
2022-05-03 01:25:01 +0100

    add countdown to quizzesWrapper component

--- src/components/QuizzesWrapper.js
+++ src/components/QuizzesWrapper.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { Children, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { add } from "../store/quizzesResultSlice";
 import QuizBox from "../components/QuizBox";
 import Alert from "react-bootstrap/Alert";
@@ -30,7 +31,7 @@ const QuizzesWrapper = ({ quizzes, difficulty, topic }) => {
 
   return (
     <div>
-      <Alert variant="success">
+      <Alert variant="success" className="text-center">
         <Alert.Heading className="text-center">Information</Alert.Heading>
         <hr />
         <h6>Difficulty: {difficulty}</h6>
@@ -40,6 +41,22 @@ const QuizzesWrapper = ({ quizzes, difficulty, topic }) => {
         <h6>
           Question number: {currentIndex + 1} /{quizzes.length}
         </h6>
+        <hr />
+        <div className="w-100 d-flex justify-content-center">
+          <CountdownCircleTimer
+            isPlaying
+            duration={1200}
+            colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+            colorsTime={[7, 5, 2, 0]}
+          >
+            {({ remainingTime }) => {
+              const minutes = Math.floor(remainingTime / 60);
+              const seconds = remainingTime % 60;
+
+              return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+            }}
+          </CountdownCircleTimer>
+        </div>
       </Alert>
       {quiz && <QuizBox quiz={quiz} handleChangeQuiz={handleChangeQuiz} />}
     </div>
