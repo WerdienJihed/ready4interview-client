0edba53f1e61039d426ddc7881602af07a9bb15c
werdien jihed <informatique.jihed@gmail.com>
2022-05-03 02:03:27 +0100

    change Quiz wrapper layout to display informations as flex

--- src/components/QuizzesWrapper.js
+++ src/components/QuizzesWrapper.js
@@ -1,10 +1,12 @@
-import { Children, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { add } from "../store/quizzesResultSlice";
 import QuizBox from "../components/QuizBox";
 import Alert from "react-bootstrap/Alert";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const QuizzesWrapper = ({ quizzes, difficulty, topic }) => {
   const [quiz, setQuiz] = useState(null);
@@ -31,33 +33,30 @@ const QuizzesWrapper = ({ quizzes, difficulty, topic }) => {
 
   return (
     <div>
-      <Alert variant="success" className="text-center">
-        <Alert.Heading className="text-center">Information</Alert.Heading>
-        <hr />
-        <h6>Difficulty: {difficulty}</h6>
-        <hr />
-        <h6>Topic: {topic}</h6>
-        <hr />
-        <h6>
-          Question number: {currentIndex + 1} /{quizzes.length}
-        </h6>
-        <hr />
-        <div className="w-100 d-flex justify-content-center">
-          <CountdownCircleTimer
-            isPlaying
-            duration={1200}
-            colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
-            colorsTime={[7, 5, 2, 0]}
-          >
+      <div className="d-flex my-5">
+        <Alert className="w-25">
+          <h6>Difficulty</h6> <p>{difficulty} </p>
+          <hr />
+          <h6>Topic</h6> <p>{topic}</p>
+        </Alert>
+        <div className="w-100 d-flex justify-content-center mb-4">
+          <CountdownCircleTimer isPlaying duration={1200} colors="#F7B801">
             {({ remainingTime }) => {
               const minutes = Math.floor(remainingTime / 60);
               const seconds = remainingTime % 60;
-
               return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
             }}
           </CountdownCircleTimer>
         </div>
-      </Alert>
+        <Alert
+          variant="success"
+          className="w-25 d-flex align-items-center justify-content-center"
+        >
+          <h1>
+            {currentIndex + 1} /{quizzes.length}
+          </h1>
+        </Alert>
+      </div>
       {quiz && <QuizBox quiz={quiz} handleChangeQuiz={handleChangeQuiz} />}
     </div>
   );
